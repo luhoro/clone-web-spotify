440aef4df24afc779bb4bd921e89a2198fbdc7fb
luhoro <luisashorowitz@gmail.com>
2023-06-18 23:32:28 -0300

    add files

---
+++ tailwind.config.js
@@ -0,0 +1,36 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  tailwindConfig: './styles/tailwind.config.js',
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+    extend: {
+      spacing: {
+        '14': '3.5rem',
+        '125px': '125px'
+      },
+      colors: {
+        'hoverspt': '#18d760',
+        'purple-main': '#2d46b9',
+        'green-main': '#1ed760'
+      },
+      backgroundImage: theme => ({
+        'spotify-theme': "url('/src/img/bursts.svg')",
+        'spotify-theme-mobile': "url('/src/img/bursts-mobile.svg')"
+      }),
+      backgroundSize: {
+        '175%': '175%',
+        '195%': '195%'
+      },
+      backgroundPosition: {
+        'banner': '46% 4%',
+        'banner-mobile': 'top 25% center'
+      },
+      fontSize: {
+        '9xl': '9rem'
+      }
+    }
+  },
+  plugins: [ require('prettier-plugin-tailwindcss') ],
+}
+
